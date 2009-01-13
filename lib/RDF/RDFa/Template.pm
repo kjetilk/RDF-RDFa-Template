d7565db8f0f5be2fa95411b462b6635ca98b3d8f
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2009-01-13 18:15:40 +0000

    Initial import
    git-svn-id: http://svn.kjernsmo.net/RDF-RDFa-Template/trunk@248 c0e5d59b-64f6-0310-af01-800fc5e6926e

---
+++ lib/RDF/RDFa/Template.pm
@@ -0,0 +1,107 @@
+package RDF::RDFa::Template;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+RDF::RDFa::Template - The great new RDF::RDFa::Template!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use RDF::RDFa::Template;
+
+    my $foo = RDF::RDFa::Template->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Kjetil Kjernsmo, C<< <kjetilk at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rdf-rdfa-template at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RDF-RDFa-Template>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc RDF::RDFa::Template
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RDF-RDFa-Template>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/RDF-RDFa-Template>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/RDF-RDFa-Template>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/RDF-RDFa-Template/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Kjetil Kjernsmo, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of RDF::RDFa::Template
