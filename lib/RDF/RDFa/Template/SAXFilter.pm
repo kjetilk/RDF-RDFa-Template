09f0da5020f0ee579e8f623827d38593d8efb65b
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2010-01-13 23:34:39 +0000

    try to redo stuff to use a stack
    git-svn-id: http://svn.kjernsmo.net/RDF-RDFa-Template/trunk@374 c0e5d59b-64f6-0310-af01-800fc5e6926e

--- lib/RDF/RDFa/Template/SAXFilter.pm
+++ lib/RDF/RDFa/Template/SAXFilter.pm
@@ -20,6 +20,7 @@ sub new {
     my %options = @_;
     $options{BaseURI} ||= './';	    
     $options{_graphstack} = [];
+    $options{_is_in_graph} = 0;
     return bless \%options, $class;
 }
 
@@ -31,20 +32,25 @@ sub start_element {
     if ($element->{LocalName} eq 'graph') {
       # This element should not be sent to the result document,
       # but its contents should be looped and variables substituted
-      $self->{_currentgraph} = $element->{Attributes}->{'g:graph'}->{Value};
-      die "couldn't find current graph name" unless $self->{_currentgraph}; # TODO: Fix, don't require hardcoding
+      $self->{_currentgraph} = $element->{Attributes}->{'g:graph'}->{Value}; # TODO: Fix, don't require hardcoding
+      if (defined($self->{_currentgraph})) {
+	$self->{_is_in_graph} = 1;
+      }
+      die "couldn't find current graph name" unless $self->{_currentgraph};
       $self->{_results} = $self->{Doc}->unit($self->{Doc}->{PARSED}->uri . $self->{_currentgraph})->results; # TODO: PARSED method
     } elsif ($element->{LocalName} eq 'variable') {
       my ($var) = $element->{Attributes}->{name}->{Value} =~ m/sub:(\w+)/; # TODO: Don't hardcode sub-prefix
       my $binding = $self->{_results}->binding_value_by_name($var);
-      $self->SUPER::characters({Data => $binding}); # TODO: I get a literal, not just a simple string;
+      $self->{_graph_stack}}->[-1]->{character_data} .= $binding; # TODO: I get a literal, not just a simple string;
     }
-  } elsif ($element->{Attributes}->{about} 
+  } elsif ($self->{_is_in_graph} && $element->{Attributes}->{about} 
 	   && ($element->{Attributes}->{about}->{Value} eq 'sub:resource')) { # TODO: coded to the test
     my $uri = $self->{_results}->binding_value_by_name('resource');
     $uri =~ s/^<(.*)>$/$1/;
     $element->{Attributes}->{about}->{Value} = $uri;
-    $self->SUPER::start_element($element);
+    push (@{$self->{_graph_stack}}, $element);
+  } elsif ($self->{_is_in_graph}) {
+    push (@{$self->{_graph_stack}, $element);
   } else {
     $self->SUPER::start_element($element);
   }
@@ -56,6 +62,7 @@ sub end_element {
   if ($element->{NamespaceURI} eq $self->{Doc}->{RATURI}) {
     if ($element->{LocalName} eq 'graph') {
       $self->{_currentgraph} = undef;
+      $self->{_is_in_graph} = 0;
       $self->{_results} = undef;
     }
   } else {
